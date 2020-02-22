b86d5c484ee9bef46898d75920dc034f9f5fa2c5
Ostap Klymchuk <oklymchuk@gmail.com>
2020-02-22 18:58:59 +0200

    Add close session and connection to db

--- migrator.go
+++ migrator.go
@@ -29,5 +29,10 @@ func main() {
 		fmt.Printf("There is some problem with getting data from database: %v\n", err)
 		return
 	}
+	err = MigratorManager.CloseConnection()
+	if err != nil {
+		fmt.Printf("Couldn't close connection to PostgreSQL database: %v\n", err)
+		return
+	}
 	fmt.Println("Thank you! Data were migrated from PostgreSQL to Cassandra!")
 }
